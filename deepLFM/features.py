9f6a6c22a1af0ef949607603352b1d3f07c1bb97
Magnus Ross <mross1@sheffield.ac.uk>
2021-09-03 16:57:36 +0100

    bug fixed

--- deepLFM/features.py
+++ deepLFM/features.py
@@ -66,7 +66,7 @@ class NPFeatures:
         self.z = z
         self.alph = alph
         self.ls = ls
-        self.amp = amp
+        self.amp = 1.0
         self.Nbasis = Nbasis
         self.Ns = Ns
         self.set_K()
@@ -94,42 +94,53 @@ class NPFeatures:
         self.K = K
         self.LK = LK
 
-    def sample_basis(self):
-        thets = torch.randn(self.Ns, self.Nbasis) / self.ls
+    def sample_basis(self, Ns=None):
+        if Ns is None:
+            Ns = self.Ns
+
+        thets = torch.randn(Ns, self.Nbasis) / self.ls
         ws = (
             self.amp
             * torch.sqrt(torch.tensor(2.0 / self.Nbasis, requires_grad=False))
-            * torch.randn(self.Ns, self.Nbasis)
+            * torch.randn(Ns, self.Nbasis)
         )
-        betas = 2 * pi * torch.rand(self.Ns, self.Nbasis)
+        betas = 2 * pi * torch.rand(Ns, self.Nbasis)
         return thets, betas, ws
 
     def compute_Phi(self, thets, betas, ws, ts=None):
         if ts is None:
             ts = self.z
-        N = ts.shape[0]
-        rthets = thets.unsqueeze(-1).repeat(1, 1, N)
-        rbetas = betas.unsqueeze(-1).repeat(1, 1, N)
-        rts = ts.unsqueeze(0).unsqueeze(0).repeat(self.Ns, self.Nbasis, 1)
-        return torch.cos(rthets * rts + rbetas)
+        return torch.cos(thets[:, :, None] * ts[None, None, :] + betas[:, :, None])
 
     def compute_q(self, thets, betas, ws):
-
         phi = self.compute_Phi(thets, betas, ws)
         us = self.u_dist.rsample(sample_shape=(self.Ns,))
-        rLK = self.K.unsqueeze(0).repeat(self.Ns, 1, 1)
+        rLK = self.LK.unsqueeze(0).repeat(self.Ns, 1, 1)
         x = us.unsqueeze(-1) - phi.transpose(1, 2).matmul(ws.unsqueeze(-1))
         return torch.cholesky_solve(x, rLK)
 
     def sample_G(self, ts):
         thets, betas, ws = self.sample_basis()
         qs = self.compute_q(thets, betas, ws)
+        # these are for the ouput not ips
+        # reuqired cos function is vectorised over samples
         phis = self.compute_Phi(thets, betas, ws, ts=ts)
+        print(phis.shape)
         K, _ = self.compute_K(t1s=ts, t2s=self.z)
-        print(K.shape)
-        print(qs.shape)
+
         basis_part = qs.squeeze(-1).matmul(K.T)
         rws = ws.unsqueeze(-1).repeat(1, 1, ts.shape[0])
         random_part = torch.sum(rws * phis, 1)
+        return torch.exp(-self.alph * ts[None, :] ** 2) * (basis_part + random_part)
 
-        return random_part + basis_part
+    def sample_features(self, ts, omegas, Ns):
+        """
+        for now we say that for each omega sample we also sample from the filter
+        omegas = Nrff
+        ts = Nt
+        """
+        # Nt x Ns (Ns = Nb x Nq)
+        thets, betas, ws = self.sample_basis(Ns=Ns)
+        # qs = self.compute_q(thets, betas, ws)
+
+        # mI1 = I1(ts[None, None, :])
--- new_demo.py
+++ new_demo.py
@@ -4,30 +4,30 @@ import torch
 import matplotlib.pyplot as plt
 from torch.distributions import MultivariateNormal, kl_divergence
 
-
 # %%
-z = torch.linspace(0, 1, 50)
-mu = 10 * torch.ones(50, requires_grad=True)
-LK = 0.1 * torch.eye(50, requires_grad=True)
+z = torch.linspace(-2, 2, 5)
+mu = 1.0 * z
+LK = 0.0000001 * torch.eye(5, requires_grad=True)
 u_dist = MultivariateNormal(mu, scale_tril=LK)
 
 
-features = NPFeatures(u_dist, z, 1.0, 1.0, 1.0, 100, 10)
-
 ls = torch.tensor([0.2], requires_grad=True)
-features.ls = ls
-K = features.compute_K()
-ans = torch.sum(K[0])
-ans.backward()
-ls.grad
+features = NPFeatures(u_dist, z, 1.0, ls, 1.0, 98, 99)
+
+#
+# #
 
 
-x = torch.linspace(-1, 1, 200)
+x = torch.linspace(-3, 3, 100)
 samps = features.sample_G(x)
+ans = torch.sum(samps)
+ans.backward()
+print(ls.grad)
+
 plt.plot(x, samps.T.detach().numpy())
+
+
 # %%
-
-
 kl_divergence(u_dist, u_dist)
 # %%
 features.Nu
